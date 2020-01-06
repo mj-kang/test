f810d6cb75245349933ff57a09bc2dda45c61ab5
kmj <kangmg1017@info-works.co.kr>
2020-01-06 15:41:37 +0900

    new file:   prac1/CamWithV4L2.cpp 	new file:   prac1/CamWithV4L2.h 	new file:   prac1/Makefile 	new file:   prac1/cam_V4L2 	new file:   prac1/main.cpp 	deleted:    prac

---
+++ prac1/CamWithV4L2.h
@@ -0,0 +1,41 @@
+#ifndef CAMWITHV4L2_H
+#define CAMWITHV4L2_H
+
+#include <linux/videodev2.h>
+#include <stdint.h>
+#include <iostream>
+struct buffer
+{
+        void *start;
+        size_t length;
+};
+
+class CamWithV4L2
+{
+public:
+    CamWithV4L2();
+    CamWithV4L2(int _num, int _width, int _height, struct v4l2_pix_format _pixel_fmt);
+    ~CamWithV4L2();
+
+    int Open_Device(void);
+    void Init_Device(void);
+    void Init_mmap(void);
+    void Start_Capturing(void);
+    void MainLoop(void);
+    void Stop_Capturing(void);
+    void Uninit_Device(void);
+    void Close_Device(void);
+    int read_frame(void);
+    void Processing(void * start, uint32_t bytesused);
+
+    struct buffer * buffers;    
+    int dev_num;
+    int width;
+    int height;
+    struct v4l2_pix_format  pixel_fmt;
+private:
+    int fd;
+    int nbuffers;
+    int thread_exit;
+};
+#endif //CAMWITHV4L2_H
